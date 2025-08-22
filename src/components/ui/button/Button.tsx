2111fdb5609c56dc7d064568585d0a76f9499e89
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-08-22 23:50:35 +0200

    Adjust button styling

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
         variants: {
             variant: {
                 default:
-                    'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+                    'bg-primary text-primary-foreground shadow hover:bg-gray-100',
                 destructive:
                     'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
                 outline:
